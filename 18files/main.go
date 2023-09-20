40ece8f383f79a5959cabf1c1c16719038726f78
Imran Hossain <ih060899@gmail.com>
2023-09-20 20:37:02 +0600

    files in golang

--- 18files/main.go
+++ 18files/main.go
@@ -11,18 +11,24 @@ func main() {
 	content := "This needs to go in file - LeanCodeOnline.in"
 	file, err := os.Create("./mylcogofile.txt")
 
-	if err != nil {
-		panic(err)
-	}
+	checkNilErr(err)
 	length, err := io.WriteString(file, content)
 
-	if err != nil {
-		panic(err)
-	}
+	checkNilErr(err)
 	fmt.Println("length is: ", length)
 	defer file.Close()
+	readFile("./mylcogofile.txt")
 }
 
 func readFile(fileName string) {
+	//databyte, err := ioutil.ReadFile(fileName)
+	databyte, err := os.ReadFile(fileName)
+	checkNilErr(err)
+	fmt.Println("Text data inside the file is \n", string(databyte))
+}
 
+func checkNilErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
