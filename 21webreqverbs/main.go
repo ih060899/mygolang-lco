58719b6285a3d62710ca714401b3ffbb8b43cabe
Imran Hossain <ih060899@gmail.com>
2023-09-23 11:55:41 +0600

    post request in golang

--- 21webreqverbs/main.go
+++ 21webreqverbs/main.go
@@ -9,10 +9,11 @@ import (
 
 func main() {
 	fmt.Println("Welcome to web verb video - LCO")
-	PerfromGetRequest()
+	//PerformGetRequest()
+	PerformPostJsonRequest()
 }
 
-func PerfromGetRequest() {
+func PerformGetRequest() {
 	const myurl = "http://localhost:8000/get"
 	response, err := http.Get(myurl)
 	if err != nil {
@@ -31,4 +32,26 @@ func PerfromGetRequest() {
 
 	//fmt.Println(content)
 	//fmt.Println(string(content))
+}
+
+func PerformPostJsonRequest() {
+	const myurl = "http://localhost:8000/post"
+
+	// fake json payload
+
+	requestBody := strings.NewReader(`
+{
+"coursename":"Let's go with golang.'",
+"price": 0,
+"platform": "learnCodeOnline.in"
+}
+`)
+	response, err := http.Post(myurl, "application/json", requestBody)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	content, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(content))
 }
