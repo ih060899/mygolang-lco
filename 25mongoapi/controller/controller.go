02d5642c7e62cfcd3823771b7fd8ec3c925b3204
imran <ih060899@gmail.com>
2023-09-26 15:14:08 +0600

    init1 mongo api

--- 25mongoapi/controller/controller.go
+++ 25mongoapi/controller/controller.go
@@ -3,6 +3,9 @@ package controller
 import (
 	"context"
 	"fmt"
+	"github.com/ih060899/mongoapi/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
@@ -27,4 +30,41 @@ func init() {
 		log.Fatal(err)
 	}
 	fmt.Println("MongoDB connection success")
+
+	collection = client.Database(dbName).Collection(colName)
+
+	// collection instance
+	fmt.Println("Collection Instance is ready.")
+}
+
+// MONGODB helpers - file
+
+// insert 1 record
+
+func insertOneMovie(movie model.Netflix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Insert 1 movie in db with id: ", inserted.InsertedID)
+}
+
+// update one record
+
+func updateOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("modified count: ", result.ModifiedCount)
+}
+
+// Delete 1 record
+func deleteOneMo() {
+
 }
